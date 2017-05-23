d0da64a127c61c04fe2e1934d9cdd8d1757499a3
Himanshu Tiwari <ithimanshutiwari@gmail.com>
2017-05-23 13:15:52 +0530

    Inroduction

---
+++ tree/Sol1.java
@@ -0,0 +1,24 @@
+/*
+1. Binary tree introduction
+http://www.geeksforgeeks.org/binary-tree-set-1-introduction/
+*/
+class Node{
+	int item;
+	Node left,right;
+
+	public Node(int item){
+		this.item=item;
+		left=null;
+		right=null;
+	}
+
+}
+
+class Sol1{
+
+	
+	public static void main(String[] args){
+			Node root = new Node(1);
+			System.out.println(root.item+" "+root.left);
+	}
+}
